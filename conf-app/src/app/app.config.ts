0228d76d18eae6c87187919aa6cf25da3a122695
Akinbode Abiogun <abitoguna@gmail.com>
2024-04-12 14:04:19 +0100

    Update AppConfig to use withHashLocation and paramsInheritanceStrategy in router provider
    * Change from `provideRouter(routes)` to `provideRouter(routes, withHashLocation(), withRouterConfig({ paramsInher

--- conf-app/src/app/app.config.ts
+++ conf-app/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation, withRouterConfig } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 
 import { routes } from './app.routes';
@@ -14,7 +14,7 @@ export function tokenGetter () {
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation(), withRouterConfig({ paramsInheritanceStrategy: 'always'})),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     importProvidersFrom(
